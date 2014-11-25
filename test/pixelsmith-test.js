4bb9326ebcb982821dc318a42ca594355c6d0e6e
Todd Wolfson <todd@twolfson.com>
2014-11-24 23:35:30 -0800

    Cleaning up png exclusive notes

--- test/pixelsmith-test.js
+++ test/pixelsmith-test.js
@@ -2,11 +2,6 @@
 var spritesmithEngineTest = require('spritesmith-engine-test');
 var pixelsmith = require('../');
 
-// Override images with png variants
-// TODO: Define as a flag inside of `spritesmith-engine-test`
-// spritesmithEngineTest.config.multipleImages = spritesmithEngineTest.config.multiplePngImages;
-// spritesmithEngineTest.config.repeatingImages = spritesmithEngineTest.config.repeatingPngImages;
-
 // Run our test
 spritesmithEngineTest.run({
   engine: pixelsmith,
