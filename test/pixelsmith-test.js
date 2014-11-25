24041634d335f23c710fc5802279ed3243e5c7d0
Todd Wolfson <todd@twolfson.com>
2014-11-25 00:45:26 -0800

    Defined custom tests for jpeg and gif

--- test/pixelsmith-test.js
+++ test/pixelsmith-test.js
@@ -3,9 +3,44 @@ var spritesmithEngineTest = require('spritesmith-engine-test');
 var pixelsmith = require('../');
 
 // Run our test
+// DEV: This covers png and jpeg inputs
 spritesmithEngineTest.run({
   engine: pixelsmith,
   engineName: 'pixelsmith'
 });
 
-// TODO: Should we test png, jpeg, and gif support?
+// Define custom tests
+var spritesmithUtils = spritesmithEngineTest.spritesmithUtils;
+describe('pixelsmith', function () {
+  describe('working with multiple images', function () {
+    describe('outputting a jpeg image', function () {
+      // Render a jpeg image
+      var multipleImages = spritesmithEngineTest.config.multipleImages;
+      spritesmithUtils.interpretImages(pixelsmith, multipleImages.filepaths);
+      spritesmithUtils.renderCanvas({
+        engine: pixelsmith,
+        width: multipleImages.width,
+        height: multipleImages.height,
+        coordinateArr: multipleImages.coordinateArr,
+        exportParams: {
+          format: 'jpeg'
+        }
+      });
+
+      // Allow for debugging
+      if (process.env.TEST_DEBUG) {
+        spritesmithUtils.debugResult('debug.jpg');
+      }
+
+      it.only('outputs a jpeg image', function () {
+        // TOOD: Complete me
+      });
+    });
+
+    describe.skip('outputting a gif image', function () {
+      it('outputs a gif image', function () {
+        // TOOD: Complete me
+      });
+    });
+  });
+});
